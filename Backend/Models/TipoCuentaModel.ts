f44a70d71120b5f8fbd3045b0391e5b49822e970
JAVIERAVILA\javie <javieravilapinilla52@gmail.com>
2025-06-10 10:36:44 -0500

    archivos

---
+++ Backend/Models/TipoCuentaModel.ts
@@ -0,0 +1,141 @@
+import { promises } from "node:dns";
+import { z } from "..//Dependencies/";
+import { conexion } from "./Conexion.ts";
+import { error } from "node:console";
+
+interface ClienteData {
+  idUsuario: number | null;
+  nombre: string;
+  apellido: string;
+  documento: string;
+  telefono: string;
+  direccion: string;
+}
+
+export class Clientes {
+  public _objcliente: ClienteData | null;
+  public _idCLiente: number | null;
+
+  constructor(
+    objclient: ClienteData | null = null,
+    idCliente: number | null = null,
+  ) {
+    this._objcliente = objclient;
+    this._idCLiente = idCliente;
+  }
+
+  public async SeleccionarCliente(): Promise<ClienteData[]> {
+    const { rows: client } = await conexion.execute("select * from cliente");
+    return client as ClienteData[];
+  }
+
+  public async insertarCliente(): Promise<
+    { success: boolean; message: string; cliente?: Record<string, unknown> }
+  > {
+    try {
+      //validar que _objUsuario no sea null y que los campos requeridos esten definidos
+      if (!this._objcliente) {
+        throw new Error("No se ha proporcionado un objeto de usuario valido");
+      }
+
+      const { nombre, apellido, documento, telefono, direccion } = this._objcliente;
+      if (!nombre || !apellido || !documento || !telefono || !direccion) {
+        throw new Error("Faltan campos requeridos para insertar el usuario");
+      }
+
+      await conexion.execute("START TRANSACTION");
+
+      const result = await conexion.execute(
+        `insert into cliente(nombre,apellido,documento,telefono,direccion)values(?,?,?,?,?)`,
+        [
+          nombre,
+          apellido,
+          documento,
+          telefono,
+          direccion,
+        ],
+      );
+
+      if (
+        result && typeof result.affectedRows === "number" &&
+        result.affectedRows > 0
+      ) {
+        const [cliente] = await conexion.query(
+          `select * from cliente WHERE idUsuario = LAST_INSERT_ID()`,
+        );
+
+        await conexion.execute("COMMIT");
+
+        return {
+          success: true,
+          message: "cliente registrado correctamente.",
+          cliente: cliente,
+        };
+      } else {
+        throw new Error("no fue posible registrar el usuario.");
+      }
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        return { success: false, message: error.message };
+      } else {
+        return { success: false, message: "Error interno del servidor" };
+      }
+    }
+  }
+
+ public async actualizarClientes(): Promise<{ success: boolean; message: string }> {
+    try {
+      if (!this._objcliente || !this._objcliente.idUsuario) {
+        throw new Error("Datos de usuario no válidos");
+      }
+  
+      const { idUsuario, nombre, apellido, documento, telefono, direccion } = this._objcliente;
+  
+      await conexion.execute("START TRANSACTION");
+  
+      const result = await conexion.execute(
+        `UPDATE cliente SET nombre = ?, apellido = ?, documento = ?, telefono = ?, direccion = ? WHERE idUsuario = ?`,
+        [nombre, apellido, documento, telefono, direccion, idUsuario]
+      );
+  
+      if (result && typeof result.affectedRows === "number" && result.affectedRows > 0) {
+        await conexion.execute("COMMIT");
+        return { success: true, message: "cliente actualizado correctamente." };
+      } else {
+        throw new Error("No se encontró el cliente o no se pudo actualizar.");
+      }
+  
+    } catch (error) {
+      await conexion.execute("ROLLBACK");
+      return { success: false, message: "Error al actualizar el usuario." };
+    }
+  }
+
+ public async eliminarCliente(): Promise<{ success: boolean; message: string }> {
+    try {
+      if (!this._idCLiente) {
+        throw new Error("ID de usuario no proporcionado");
+      }
+  
+      await conexion.execute("START TRANSACTION");
+  
+      const result = await conexion.execute(
+        "DELETE FROM cliente WHERE idUsuario = ?",
+        [this._idCLiente]
+      );
+  
+      if (result && typeof result.affectedRows === "number" && result.affectedRows > 0) {
+        await conexion.execute("COMMIT");
+        return { success: true, message: "Usuario eliminado correctamente." };
+      } else {
+        throw new Error("No se encontró el usuario o no se pudo eliminar.");
+      }
+  
+    } catch (error) {
+      await conexion.execute("ROLLBACK");
+      return { success: false, message: "Error al eliminar el usuario." };
+    }
+  }
+
+
+}
