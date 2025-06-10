ff099f13004c15c30a9642d8e783d2d6361827a2
ProgramJhoimar <navarrojhoimar@gmail.com>
2025-06-10 09:00:23 -0500

    Dependencias actualizadas

---
+++ Backend/Dependencies/Dependencias.ts
@@ -0,0 +1,4 @@
+export { Application , Router } from "https://deno.land/x/oak@v17.1.4/mod.ts"; //Dependencia para montar el servidor.
+export { Client } from "https://deno.land/x/mysql@v2.12.1/mod.ts"; //Dependencia para conectarme a una bd SQL.
+export { z } from "https://deno.land/x/zod@v3.24.4/mod.ts"; //Dependencia para detectar errores.
+export { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts"; //Dependencia para conectar Front-end con Back-end
