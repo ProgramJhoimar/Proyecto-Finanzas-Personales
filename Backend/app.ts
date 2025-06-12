2b54f79c4e39b0ef971b6b686f3203d55dd25e01
JAVIERAVILA\javie <javieravilapinilla52@gmail.com>
2025-06-12 07:33:42 -0500

    correcion 2.0

--- Backend/app.ts
+++ Backend/app.ts
@@ -3,8 +3,6 @@ import { routerCategoria } from "./Routes/CategoriaRouter.ts";
 import { routerCuenta } from "./Routes/CuetaRouter.ts";
 import { RouterTipo } from "./Routes/TipoCuentaRouter.ts";
 import { routeTipoFinanza } from "./Routes/TipoFinanzaRouter.ts";
-import { routeTipoFinanza } from "./Routes/TipoFinanzaRouter.ts";
-import { RouterTipo } from "./Routes/TipoCuentaRouter.ts";
 
 
 
