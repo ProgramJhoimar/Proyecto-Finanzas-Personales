b6a5ae029057907e4cf0129e30e241aef3ec4202
JAVIERAVILA\javie <javieravilapinilla52@gmail.com>
2025-06-13 06:34:11 -0500

    correcion app.ts y agregacion de variables de Ruta

--- Backend/app.ts
+++ Backend/app.ts
@@ -15,8 +15,8 @@ const app = new Application();
 
 app.use(oakCors());
 
-const routers = [routerCategoria, routerCuenta, routeTipoFinanza, UserRouter, RouterTipo,routerDetallesT];
-const routers = [routerCategoria, routerCuenta, routeTipoFinanza, UserRouter, RouterTipo, RouterTransaccion];
+
+const routers = [routerCategoria, routerCuenta, routeTipoFinanza, UserRouter, RouterTipo, RouterTransaccion,routerDetallesT];
 app.use(async (ctx, next) => {
   if (ctx.request.url.pathname.startsWith("/Uploads")) {
     await send(ctx, ctx.request.url.pathname, {
